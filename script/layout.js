35812b3614c6c88b445aefdbd6a2a69fd1705f9c
Jason Duff <2jduff@gmail.com>
2020-10-28 11:59:57 -0400

    fixed nav on scroll
    Mis css changes and fixed nav on scroll

--- script/layout.js
+++ script/layout.js
@@ -1,1 +1,21 @@
 
+function headerStuff() {
+    var y = window.pageYOffset;
+    var pxDown = 130;
+
+    if(y > pxDown && $(window).width() > 768) {
+        $('#mainnav').addClass('fixed');
+        $('#main').css('margin-top', '39px');
+    } else {
+        $('#mainnav').removeClass('fixed');
+        $('#main').css('margin-top', '0px');
+    }
+}
+
+$(window).on('scroll', function() {
+    headerStuff();
+});
+
+$(window).on("resize",function(e){
+    headerStuff();
+});
